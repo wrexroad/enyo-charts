303f4335f68af8401ee38f07ddfc8843873cd296
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2015-02-02 16:15:36 -0800

    started implementing more functions for the abstract Chart kind and the Cartesian subkind

--- Cartesian.js
+++ Cartesian.js
@@ -1,13 +1,108 @@
 enyo.kind({
   name: "Chart.Cartesian",
   kind: "Chart",
-  xSpacingFactor: 1,
-  ySpacingFactor: 1,
 
-  calculateSpacing: function() {
-
+  published: {
+    xSpacingFactor: 1,
+    ySpacingFactor: 1
   },
-  drawData: function(){
-     
+
+  drawData: function() {
+    
+  },
+  drawDecor: function() {
+    var
+      ctx = this.decorCtx,
+      plotRegion = this.plotRegion;
+
+    //configure the drawing context
+    ctx.save();
+    ctx.strokeStyle = this.borderColor;
+    ctx.textAlign = "end";
+
+    //outline the grid
+    ctx.translate(plotRegion.left, plotRegion.top);
+    ctx.strokeRect(0, 0, this.width, this.height);
+
+    //draw the y axis tics and labels
+    ctx.translate(0, this.height);
+    
+    labels =
+      Dave_js.Utils.createLabels(
+        this.range.yMin,
+        this.range.yMax,
+        y
+      );
+    numLabels = labels.length;
+
+    for (pnt_i = 0, ticLocation = 0; pnt_i < numLabels; pnt_i ++) {
+      ticLocation = -labels[pnt_i].coord * this.spacing.y;
+      ctx.fillText(labels[pnt_i].text, -5, ticLocation + 5);
+      ctx.beginPath();
+      ctx.moveTo(0, ticLocation);
+      ctx.lineTo(5, ticLocation);
+      ctx.stroke();
+    }
+
+    //draw the x axis tics and labels
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+
+    labels =
+      Dave_js.Utils.createTimeLabels(
+        this.range.xMin,
+        this.range.xMax,
+        dataStore.getVar(vars.x)
+      );
+      
+    numLabels = labels.length;
+    halfWidth =
+      Math.ceil(ctx.measureText((new Array(x.labelLength)).join('W')).width);
+
+    for (pnt_i = 0, ticLocation = 0; pnt_i < numLabels; pnt_i++) {
+      labelText = labels[pnt_i].text;
+      ticLocation = labels[pnt_i].coord * this.spacing.x;
+
+      if(ticLocation > lastText) {
+        ctx.fillText(labelText, ticLocation, fontSize);
+        lastText = ticLocation + halfWidth;
+        ticLength = -15;
+      } else {
+        ticLength = -5;
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(ticLocation, 0);
+      ctx.lineTo(ticLocation, ticLength);
+      ctx.stroke();
+    }
+    ctx.restore();
+  },
+  calculateMargins: function() {
+    this.set("plotRegion", {
+      //room for the title
+      top: this.fontSize,
+      
+      //room for two lines of x axis lables
+      bottom: this.fontSize << 1,
+
+      //y axis lables can be 10 charcters wide
+      left: this.decorCtx.measureText((new Array(10)).join('W')).width,
+
+      //meh
+      right: 0
+    });
+  },
+  calculateSpacing: function() {
+    var data = this.dataCache;
+
+    this.set("xSpacingFactor", this.width / (data.xMax - data.xMin));
+    this.set("ySpacingFactor", this.height / (data.yMax - data.yMin));
+  },
+  invertCoodinates: function(x, y) {
+    return {
+      x: this.xMin + x / this.xSpacingFactor,
+      y: this.yMax - y / this.ySpacingFactor
+    };
   }
 });
--- Chart.js
+++ Chart.js
@@ -1,21 +1,24 @@
 enyo.kind({
   name: "Chart",
-  kind: "Component",
+  kind: "Control",
   
   published: {  
-    height: 0,
     width: 0,
-    plotRegion: 0,
+    height: 0,
     bgImg: "", //id of <img> tag to be used as a background
     bgColor: '#CCCCCC',
     borderColor: '#000000',
     gridColor: "gray",
     textColor: "black",
-    cssFont: '14px "Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace',
-    dataCache: null
+    fontName: '"Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace',
+    fontSize: 14,
+    decorCtx: null,
+    dataCtx: null,
+    dataCache: null,
+    dataRegion: null
   },
   observers: [
-    {method: "resize", path: ["height", "width"]},
+    {method: "resize", path: ["height", "width", "fontSize"]},
     {method: "calculateSpacing", path: ["dataCache"]}
   ],
 
@@ -24,16 +27,12 @@ enyo.kind({
     {name: "dataCanvas", kind: "enyo.Canvas"}
   },
 
-  constructor: function() {
+  //functions directly related to generating the plot
+  rendered: function() {
     this.inherited(arguments);
-
-    //plotRegion defines the bounds of the data canvas relative to the decor
-    this.plotRegion = {
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0
-    };
+    
+    this.decorCtx = this.$.decorCanvas.node.getContext('2d');
+    this.dataCtx  = this.$.dataCanvas.node.getContext('2d');
 
     //default x and y origins for the canvas coordinate system   
     this.origin = {x : 60, y : 20};
@@ -42,13 +41,19 @@ enyo.kind({
   },
   resize: function(){
     
+    //figure out where the data region is
+    this.calculateMargins();
+    if (!this.dataRegion) {
+      return false;
+    }
+
     //adjust the size of each canvas
     this.$.decorCanvas.attributes.height = this.height;
     this.$.decorCanvas.attributes.width = this.width;
     this.$.dataCanvas.attributes.height =
-      this.height - this.plotRegion.top - this.plotRegion.bottom;
+      this.height - this.dataRegion.top - this.dataRegion.bottom;
     this.$.dataCanvas.attributes.width =
-      this.width - this.plotRegion.left - this.plotRegion.right;
+      this.width - this.dataRegion.left - this.dataRegion.right;
 
     //clear the canvases
     this.$.decorCanvas.destroyClientControls();
@@ -61,6 +66,8 @@ enyo.kind({
       this.drawData();
       this.drawDecor();
     }
+
+    return true;
   },
   draw: function(chartData) {
     this.set("dataCache", chartData);
@@ -68,9 +75,42 @@ enyo.kind({
     this.drawDecor();
   },
 
+  //utility functions
+  createLabels: function (min, max, varData) {
+    var
+      label_i, stepSize, converter, sigFigs, range, labels = [], numLabels = 10;
+
+    varData = varData || {};
+    converter = varData.converter || Dave_js.Converters.default;
+    sigFigs = varData.sigFigs;
+    min = new Big(min || 0);
+    max = new Big(max || 0);
+
+    //get the range of values
+    range = max.minus(min);
+    
+    //make sure there is a range
+    if(max.eq(min)){
+      console.error('Dave_js: Cannot create labels when min == max:');
+      console.error('\t(' + min + ' == ' + max + ')');
+      return [];
+    }
+
+    stepSize = range.div(numLabels);
+    for (label_i = min; label_i.lte(max); label_i = label_i.plus(stepSize)) {
+      labels.push({
+        text: converter(+label_i, sigFigs),
+        coord: +label_i.minus(min)
+      });
+    }
+
+    return labels;
+  },
+
   //abstract functions to be defined by the chart subkind
   drawData: function() {},
   drawDecor: function() {},
+  calculateMargins: function() {},
   calculateSpacing: function() {},
   invertCoordinates: function() {}
 });
