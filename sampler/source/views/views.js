2913685b7eea1d87363596407cb8c5d9744fda8d
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-03-11 10:45:23 -0800

    updaed layout. If no x values are provided, just use y index

--- sampler/source/views/views.js
+++ sampler/source/views/views.js
@@ -1,50 +1,56 @@
 enyo.kind({
 	name: "myapp.MainView",
-	kind: "FittableRows",
+	kind: "FittableColumns",
   classes: "chart-panel",
 	fit: true,
-	components:[
-		{kind: "onyx.Toolbar"},
+	components: [
     {
-      kind: "Chart.Cartesian", name: "chart", fit: true,
+      kind: "Chart.Cartesian", name: "chart",
       width: 400,
       height: 400,
-      
       bgColor: "white",
       fontSize: 14
     },
-    {kind: "FittableColumns", classes: "enyo-center", components: [
-      {kind: "onyx.Groupbox", components: [
-			  {kind: "onyx.GroupboxHeader", content: "X Coordinates"},
-			  {
-          kind: "onyx.TextArea", name: "xVals", onchange:"inputChanged"
-        },
-		  ]},
-      {kind: "onyx.Groupbox", components: [
-			  {kind: "onyx.GroupboxHeader", content: "Y Coordinates"},
-			  {
-          kind: "onyx.TextArea", name: "yVals", onchange:"inputChanged"
-        },
-		  ]},
+    {kind: "FittableRows", components: [
+      {kind: "FittableColumns", components: [
+        {kind: "onyx.Groupbox", components: [
+			    {kind: "onyx.GroupboxHeader", content: "X"},
+			    {kind: "onyx.InputDecorator", components: [{
+            kind: "onyx.TextArea", name: "xVals",
+            attributes: {cols: 20, rows: 10},
+            onchange: "draw"
+          }]},
+		    ]},
+        {kind: "onyx.Groupbox", components: [
+			    {kind: "onyx.GroupboxHeader", content: "Y"},
+			    {kind: "onyx.InputDecorator", components: [{
+            kind: "onyx.TextArea", name: "yVals",
+            attributes: {cols: 20, rows: 10},
+            onchange: "draw"
+          }]},
+		    ]}
+      ]},
       {kind: "onyx.Groupbox", components: [
 			  {kind: "onyx.GroupboxHeader", content: "Messages"},
-			  {kind: "onyx.TextArea", name: "msg"}
+			  {kind: "onyx.InputDecorator", components: [
+          {kind: "onyx.TextArea", name: "msg", attributes: {cols: 40}}
+        ]}
 		  ]}
     ]}
 	],
   rendered: function() {
     var
       twopi = Math.PI * 2,
-      twopihundredth = twopi / 100,
+      twopininety = twopi / 90,
       xVals = [],
       yVals = [];
       
     this.inherited(arguments);
     
-    //generate a 100 point sine wave
-    for (var i = 0; i < twopi; i += twopihundredth) {
-      xVals.push(360 * (i/ twopi));
-      yVals.push(Math.sin(i));
+    //generate a 90 point sine wave
+    for (var i = 0; i < twopi; i += twopininety) {
+      xVals.push((360 * (i / twopi)).toFixed(3));
+      yVals.push(Math.sin(i).toFixed(3));
     }
     //and fill the text boxes with them
     this.$.xVals.set("value", xVals.join(", "));
@@ -52,22 +58,9 @@ enyo.kind({
     
     this.draw();
   },
-	inputChanged: function(inSender, inEvent) {
-		var xVals, yVals;
-    
-    xVals = this.$.xVals.value.split(",");
-    yVals = this.$.yVals.value.split(",");
-    
-    if (xVals.length != yVals.length) {
-      this.$.msg.set("content", "X and Y value lists must be the same length");
-      return;
-    }
-    
-    this.draw();
-    this.resize();
-	},
   draw: function() {
     var
+      //turn the x and y values into an array
       xVals = this.$.xVals.getValue().split(","),
       yVals = this.$.yVals.getValue().split(","),
       datasets = [{
@@ -78,8 +71,21 @@ enyo.kind({
           coords: []
         }
       }];
+    	
+    //if x values are not provided, just use the index form the y values
+    if (this.$.xVals.value.trim() == "") {
+      yVals.forEach(function(val, i) {
+        xVals[i] = i;
+      });
+    } 
     
-    //fill the coordinate array
+    //make sure we have the same number of coordinates for x and y 
+    if (xVals.length != yVals.length) {
+      this.$.msg.set("content", "X and Y value lists must be the same length");
+      return;
+    }
+    
+    //fill the 2d coordinate array
     yVals.forEach(function(value, index) {
       datasets[0].data.coords.push([xVals[index], value]);
     })
@@ -88,5 +94,7 @@ enyo.kind({
       {/*options*/},
       {datasets: datasets}
     );
+    
+    this.resize();
   }
 });
