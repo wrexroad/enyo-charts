57391bbadd216f7efb30cb67a7e3fc155554f6d7
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-03-18 14:45:04 -0700

    multi-data now does a monte carlo approximation of Pi

--- sampler/source/views/xyscatter-multi.js
+++ sampler/source/views/xyscatter-multi.js
@@ -10,25 +10,31 @@ enyo.kind({
     ]},
     {
       kind: "Chart.Cartesian", name: "chart",
-      plotTitle: "Multi-Dataset Plot",
+      plotTitle: "Approximation of Pi",
       bgColor: "white",
       font: "sans-serif",
       fontSize: 14
     },
-    
-    {kind: "onyx.Button", content: "Add Data", ontap: "animationStep"},
-    {
-      kind: "onyx.Button", name: "animation",
-      content: "Toggle Animation", ontap: "toggleAnimation"
-    },
-    {kind: "onyx.Button", content: "Reset", ontap: "reset"},
+    {kind: "onyx.InputDecorator", style: "display: inline-block", components: [ 
+      {kind: "onyx.Button", content: "Add Data", ontap: "animationStep"},
+      {
+        kind: "onyx.Button", name: "animation",
+        content: "Toggle Animation", ontap: "toggleAnimation"
+      },
+      {kind: "onyx.Button", content: "Reset", ontap: "reset"},
+      {kind: "enyo.Group", components: [
+			  {kind: "enyo.Checkbox", name: "lines", active: true, content: "Lines"},
+        {tag: "br"},
+			  {kind: "enyo.Checkbox", name: "dots", content: "Dots"}
+      ]}
+    ]},
     {kind: "onyx.InputDecorator", components: [
       {content: "FPS: "},
-      {kind: "onyx.Input", name: "fps", attributes: {size: 10}},
-      {content: "Datasets Plotted: "},
-      {kind: "onyx.Input", name: "numSets", attributes: {size: 10}},
-      {content: "Points Plotted: "},
-      {kind: "onyx.Input", name: "numPts", attributes: {size: 10}}
+      {kind: "onyx.Input", name: "fps", attributes: {size: 5}},
+      {content: "Datasets Per Frame: "},
+      {kind: "onyx.Input", name: "numSets", attributes: {size: 5}},
+      {content: "Points Per Frame: "},
+      {kind: "onyx.Input", name: "numPts", attributes: {size: 5}}
     ]}
   ],
   published: {
@@ -38,9 +44,19 @@ enyo.kind({
   rendered: function() {
     this.inherited(arguments);
     
-    this.datasets = [];
-    
+    this.initVals();
     this.draw();
+  },
+  initVals: function() {
+    this.$.chart.plotTitle = "Approximation of Pi";
+    this.datasets = [];
+    this.numPts = 0;
+    this.elapsedTime = NaN;
+  },
+  reset: function(inSender, inEvent) {
+    this.initVals();
+    this.draw();
+    return true;
   },
   toggleAnimation: function() {
     this.animation = !this.animation;
@@ -61,34 +77,31 @@ enyo.kind({
     this.addDataset();
     this.draw();
   },
-  reset: function() {
-    this.datasets = [];
-    this.elapsedTime = NaN;
-    this.draw();
-  },
   addDataset: function() {
     var
-      twopi = Math.PI * 2,
-      twopininety = twopi / 90,
-      amplitude = ((10 - this.datasets.length) / 10),
-      coords = [[], []];
+      coords = [],
+      x, y, d, pnt_i;
     
-    //generate a 90 point sine wave that is a little shorter than the last
-    for (var i = 0; i < twopi; i += twopininety) {
-      coords.push(
-        [(360 * (i / twopi)).toFixed(3), (Math.sin(i) * amplitude).toFixed(3)]
-      );
+    for (pnt_i = 0; pnt_i < 100; pnt_i++) {
+      x = (Math.random() > 0.5 ? -1 : 1) * Math.random();
+      y = (Math.random() > 0.5 ? -1 : 1) * Math.random();
+      d = Math.hypot(x, y);
+      if (d <= 1) {
+        this.numPts++;
+        coords.push([x, y]);
+      }
     }
     
     this.datasets.push({
       options: {
         color: "#" + ((Math.random() * 0xFFFFFF) >> 0).toString(16),
         lines: {
-          size: 0.5,
+          size: this.$.lines.getActive() ? 0.5 : 0,
           fill: false
         },
         dots: {
-          size: 0
+          size: this.$.dots.getActive() ? 3 : 0,
+          fill: true
         }
       },
       data: {
@@ -98,11 +111,11 @@ enyo.kind({
     });
   },
   draw: function() {
-    this.$.chart.set("width", this.width ? (this.width - 20) : 400);
+    this.$.chart.set("width", 400);
     this.$.chart.set("height", 400);
     
     this.$.chart.draw(
-      {/*Manually set plot range here*/},
+      {xMin: -1, xMax: 1, yMin: -1, yMax: 1},
       {datasets: this.datasets}
     );
     
@@ -112,6 +125,8 @@ enyo.kind({
     var fps = (1/(this.elapsedTime / 1000)).toFixed(3);
     this.$.fps.set("value", fps);
     this.$.numSets.set("value", this.datasets.length);
-    this.$.numPts.set("value", this.datasets.length * 90);
+    this.$.numPts.set("value", this.numPts);
+    this.$.chart.plotTitle =
+      "Pi ~ " + ((this.numPts/(this.datasets.length * 100)) * 4).toFixed(5);
   }
 });
