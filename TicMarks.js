0e73382701eacad08e5fe15600ed00f829fd217c
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-03-30 16:54:27 -0700

    started working on having range tic marks

--- TicMarks.js
+++ TicMarks.js
@@ -4,9 +4,10 @@ enyo.kind({
   published: {
     min: 0,
     max: 0,
-    decimalPlaces: 0,
+    fractionDigits: 0,
     type: "",
-    tics: null
+    tics: null,
+    formatter: null
   },
   constructor: function(opts) {
     this.inherited(arguments);
@@ -14,23 +15,22 @@ enyo.kind({
     //create a place to store generated tics
     this.tics = [];
     
-    //set some defaults if the constructor was not configured
-    if (!this.type) {
-      this.type = "function";
-      this.formatFunction = function(val) {
-        return (+val).toFixed(this.decimalPlaces);
-      }
-    }
-    
     //create the internal format function
-    this._formatter = null;
+    this._format = null;
     this.setFormatter(opts.formatter || this.defaultFormat);
   },
   
-  observers: {
-    getDecimalPlaces: ["min", "max"]
+  defaultFormat: {
+    type: "function",
+    formatFunction: function(val) {
+      return (+val).toFixed(this.fractionDigits);
+    }
   },
-  getDecimalPlaces: function() {
+  
+  observers: {
+    getFractionDigits: ["min", "max"],
+  },
+  getFractionDigits: function() {
     var
       max = this.max + "",
       min = this.min + "",
@@ -39,31 +39,84 @@ enyo.kind({
       maxDecPlaces = maxDecLocation > 0 ? max.length - maxDecLocation : 0,
       minDecPlaces = minDecLocation > 0 ? min.length - minDecLocation : 0;
       
-    this.decimalPlaces = Math.max(minDecPlaces, maxDecPlaces) || 0;
+    this.fractionDigits = Math.max(minDecPlaces, maxDecPlaces) || 0;
   },
-  formatterChanged: function(oldVal) {
-    this.inherited(arguments);
-    this.setFormatter(this.formatter);
-  },
+  
   setFormatter: function(formatter) {
+    var numLabels, val_i;
+    
     if (!formatter || !formatter.type) {
-      this.setFormatter(this.defaultFormat);
+      this.setDefaultFormatter();
+      return;
     }
     this.formatter = formatter;
-    console.log(arguments);
-    console.log(this.formatter);
     
-    formatter.type = formatter
-    if (formatter.type == "continuous") {
-      this._formatter = function (val, min, max) {
-        this.formatter.format(val, min, max, formatter.options);
+    if (formatter.type == "function") {
+      this._format = function (val) {
+        //when we generate a tic mark, call the provided function with 
+        //'this' pointing a the formatter object.
+        //The function should be ready to take the parameters:
+        //value, min tic mark, and max tic mark
+        formatter.formatFunction.call(formatter, val, this.min, this.max);
       }
     } else if (formatter.type == "range") {
-      this._formatter = function (val, min, max) {
-        this.formatter.format(val, min, max, formatter.options);
+      //filter any NaNs out of the range bounds
+      formatter.values = ([].concat(formatter.values)).filter(
+        function (value) {
+          return +value ? true : false;
+        });
+      
+      //make sure there are still some valid bounds
+      if (!formatter.values.length) {
+        this.setDefaultFormater();
+        return;
+      }
+      
+      //create some labels if they are not provided
+      if (!formatter.labels || !formatter.labels.length) {
+        formatter.labels = [];
+        formatter.labels[0] = "Below " + formatter.values[0];
+        
+        numLabels = formatter.values.length - 1;
+        for (val_i = 0; val_i < numLabels; val_i++) {
+          formatter.labels[val_i + 1] =
+            formatter.values[val_i] + " - " + formatter.values[val_i + 1];
+        }
+        
+        formatter.labels[val_i + 1] = "Above " + formatter.values[val_i];
+      }
+      
+      //Search for the label for the region that this value falls under 
+      this._format = function (val) {
+        var
+          labels = this.formatter.labels,
+          numLabels = labels.length,
+          values = this.formatter.values,
+          numValues = values.length,
+          lowerValue = +values[0],
+          upperValue = +values[numValues - 1],
+          val_i;
+        
+        if (val < lowerValue) {
+          return this.formatter.labels[0];
+        } else if (val > upperValue) {
+          return this.formatter.labels[numLabels];
+        }
+        
+        for (val_i = 1; val_i < numValues; val_i++) {
+          if (val < values[val_i] && val > values[val_i - 1]) {
+            return labels[val_i];
+          }
+        }
       }
     } else if (formatter.type == "discrete") {
       
     }
+  },
+  setDefaultFormatter: function() {
+    this.setFormatter(
+      this.defaultFormat.type ?
+        this.defaultFormat : {type: "function", formatFunction: function(){}}
+    );
   }
 });
