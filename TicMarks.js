60e73bff618e591835436b31e73b677527a73f36
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-03-31 14:38:41 -0700

    we are not generating tic mard arrays! Woo!

--- TicMarks.js
+++ TicMarks.js
@@ -2,12 +2,16 @@ enyo.kind({
   name: "TicMarks",
   kind: "Component",
   published: {
-    min: 0,
-    max: 0,
+    min: null,
+    max: null,
+    range: null,
+    ticStep: null,
     fractionDigits: 0,
+    roundingDigit: 0,
     type: "",
     tics: null,
-    formatter: null
+    formatter: null,
+    numTics: 10
   },
   constructor: function(opts) {
     this.inherited(arguments);
@@ -27,19 +31,103 @@ enyo.kind({
     }
   },
   
-  observers: {
-    getFractionDigits: ["min", "max"],
+  //make sure min and max are set as Numbers
+  minChanged: function() {
+    this.min = +this.min;
   },
-  getFractionDigits: function() {
+  maxChanged: function() {
+    this.max = +this.max;
+  },
+  
+  observers: {
+    generateTics: ["min", "max", "numTics"],
+  },
+  
+  generateTics: function() {
     var
-      max = this.max + "",
-      min = this.min + "",
-      maxDecLocation = (max).indexOf("."),
-      minDecLocation = (min).indexOf("."),
-      maxDecPlaces = maxDecLocation > 0 ? max.length - maxDecLocation : 0,
-      minDecPlaces = minDecLocation > 0 ? min.length - minDecLocation : 0;
-      
-    this.fractionDigits = Math.max(minDecPlaces, maxDecPlaces) || 0;
+      multiplier, roundMin, roundMax, scaledRange,
+      oneTicsNum, halfTicsNum, fithsTicsNum, ticVal;
+    
+    if (!this.isValidRange()) {
+      return false;
+    }
+    
+    this.tics = [];
+    
+    //figure out how many meaningfull decimal places the min and max have
+    this.calcDigits();
+    
+    //get the rounded min and max
+    multiplier = Math.pow(10, this.roundingDigit);
+    roundMin = ((this.min / multiplier) >> 0) * multiplier;
+    roundMax = ((this.max / multiplier) >> 0) * multiplier;
+    
+    //figure out if we are going to be counting by 0.2, 0.5, or 1
+    scaledRange = ((this.range / multiplier) >> 0);
+    
+    oneTicsNum = Math.abs(scaledRange - this.numTics);
+    halfTicsNum = Math.abs((scaledRange << 1) - this.numTics);
+    fithsTicsNum = Math.abs((scaledRange * 5) - this.numTics);
+    
+    if (oneTicsNum < halfTicsNum) {
+      if (oneTicsNum < fithsTicsNum) {
+        this.ticStep = 1;
+      } else {
+        this.ticStep = 0.2;
+      }
+    } else {
+      if (halfTicsNum < fithsTicsNum) {
+        this.ticStep = 0.5;
+      } else {
+        this.ticStep = 0.2;
+      }
+    }
+    
+    //scale the ticStep to the rounding digit
+    this.ticStep *= multiplier;
+    
+    //generate the tic labels and locations
+    ticVal = roundMin;
+    while (ticVal <= roundMax) {
+      this.tics.push({
+        location: ticVal,
+        label: this._format(ticVal)
+      });
+      ticVal += this.ticStep;
+    }
+    
+    console.log(this.tics);
+  },
+  
+  isValidRange: function() {
+    //make sure there is a valid min and max: two numbers that are not equal.
+    if (this.min == this.max) {
+      return false;
+    } else if (!Number.isFinite(this.min + this.max)) {
+      return false;
+    }
+    
+    this.range = this.max - this.min;
+    
+    return true;
+  },
+  
+  calcDigits: function() {
+    var expRange, expDelim;
+     
+    expRange = (this.max - this.min).toExponential();
+    expDelim = expRange.indexOf("e-");
+    
+    if (expDelim > -1) {
+      //the range has a negative exponent,
+      //so we will be rounding to a fractional digit
+      this.fractionDigits = (+expRange.substring(expDelim + 2)) + 1;
+      this.roundingDigit = -(this.fractionDigits - 1);
+    } else {
+      //there is a positive exponent so we want to deal with integers
+      this.fractionDigits = 0;
+      this.roundingDigit = (+expRange.substring(expRange.indexOf("e+")+2)) - 1;
+    }
   },
   
   setFormatter: function(formatter) {
@@ -56,7 +144,7 @@ enyo.kind({
         //'this' pointing a the formatter object.
         //The function should be ready to take the parameters:
         //value, min tic mark, and max tic mark
-        formatter.formatFunction.call(formatter, val, this.min, this.max);
+        return formatter.formatFunction.call(this, val);
       }
     } else if (formatter.type == "range") {
       if (!formatter.labels) {
